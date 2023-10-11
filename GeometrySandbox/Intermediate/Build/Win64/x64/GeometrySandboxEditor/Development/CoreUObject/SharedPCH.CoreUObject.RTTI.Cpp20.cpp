1941d77481d017610fbb1e02cddff51662771c50
Denis Lesnikov <102676229+Iteey@users.noreply.github.com>
2023-10-11 09:50:41 +0300

    Animations lt right bck
    +Now all directions animated

--- GeometrySandbox/Intermediate/Build/Win64/x64/GeometrySandboxEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.37.32824
-#include "C:/Users/Dev/Documents/GitHub/UE5_GeometrySandbox/GeometrySandbox/Intermediate/Build/Win64/x64/GeometrySandboxEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h"
--- GeometrySandbox/Intermediate/Build/Win64/x64/GeometrySandboxEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "C:/Users/Dev/Documents/GitHub/UE5_GeometrySandbox/GeometrySandbox/Intermediate/Build/Win64/x64/GeometrySandboxEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Cpp20.h"
-#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
